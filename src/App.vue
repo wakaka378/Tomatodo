23b81e692042f9547df870477ca0034486de0098
wakaka378 <gebilaofl@163.com>
2023-05-21 20:36:47 +0800

    ci: prettier code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 // import { ElButton } from 'element-plus'
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 </script>
-
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -13,6 +12,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+
   <el-button>我是 ElButton</el-button>
 </template>
 
@@ -23,9 +23,11 @@ import HelloWorld from "./components/HelloWorld.vue";
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
