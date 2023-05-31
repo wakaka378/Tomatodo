f1b2b4b5892cba8fbac3cc3bd48f6f1cced3af27
wakaka378 <gebilaofl@163.com>
2023-05-31 21:54:20 +0800

    ci: 解决prettier保存不生效问题

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,18 @@ module.exports = {
   env: {
     browser: true,
     amd: true,
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
 }
